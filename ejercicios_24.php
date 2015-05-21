61291f90550c947bfc409adb7bb1f0f37b2635d2
belgrades <fcremaldc@gmail.com>
2015-05-21 13:51:47 -0430

    Create ejercicios_24.php
    Ejercicios de la página 24

---
+++ ejercicios_24.php
@@ -0,0 +1,78 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Ejercicios - 24</title>
+<h1> Ejercicios de la página 24 </h1>
+</head>
+
+<body>
+	<h2> Ejercicio 3 - Tabla de Multiplicar</h2>
+    	<?php
+			// Escogemos el n
+			$n = 9;
+			
+			//Escogemos hasta donde queremos la tabla
+			$m = 10;
+		?>
+        <h3> Tabla de Multiplicar del <?php echo $n; ?> </h3>
+        
+        <?php
+			for($i = 0; $i <= $m; $i++){
+				echo $i."*".$n."=".($i*$n)."<br>";		
+			}
+		?>
+        
+     
+    <h2> Ejercicio 4 - Factorial </h2>
+    	<?php
+			//Escogemos el n para el factorial
+			$n = 5;
+		?>
+    	<h3> El factorial de <?php echo $n; ?> es  
+        
+        <?php
+			$factorial = 1;
+			$prueba = 1;
+			for($i = $n ; $i >= 0; $i--){
+				if($i==0){
+					$factorial *= 1;	
+				}else{
+					$factorial *= $i;	
+				}		
+			}
+			
+			
+			echo $factorial;
+			
+					
+			for($i = $n ; $i >= 1; $prueba*=$i--){}
+			
+			echo "<br>".$prueba;
+		?>
+    	
+        </h3>
+    
+    <h2> Ejercicio 5 - Primos </h2>
+    	<?php
+			$inicio = 20;
+			$fin = 120;
+		?>
+    	<h3> Entre <?php echo $inicio; ?> y <?php echo $fin ?> 		</h3>
+        
+        <?php
+			for($i = $inicio; $i <= $fin; $i++){
+				$es_primo = TRUE;
+				for($j = 2; $j < $i; $j++){
+					if($i%$j==0)
+						$es_primo = FALSE;	
+				}	
+				if($es_primo){
+					echo $i." ";	
+				}
+			} 
+		?>
+		
+		
+     
+</body>
+</html>
