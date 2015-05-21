fc516a0cb94f1e2dd93282f4ec19861e8fcc9c27
Fernando Crema <fcremaldc@gmail.com>
2015-05-21 16:55:17 -0430

    Update incluir.php
    Agregamos los cálculos de las áreas de triángulos, círculos y trapecios.

--- incluir.php
+++ incluir.php
@@ -8,7 +8,46 @@
 	$base = 3;
 	$altura =5;
 	
-	echo triangulo($base, $altura);
+	echo triangulo($base, $altura)." ";
+	
+	$pi = 2*acos(0);
+	$pi2 = asin(0);
+	
+	$x = 2;
+	
+	echo pow(2, $x)." ";
+	
+	echo $pi2." ".$pi." ";
+	
+	
+	echo cos($pi*90/180). "<br>";
+	
+	$pi_val = 2*acos(0);
+	
+	
+	//usando el {area de un circulo
+	function circulo($radio){
+		$pi = 2*acos(0);
+		return $pi*$radio*$radio."<br>";
+	}
+	
+	$radio = 10;
+	
+	function trapecio($b1, $b2, $altura){
+		return ($b1 + $b2)*$altura/2;	
+	}
+	
+	echo circulo($radio)."<br>";
+	
+	$b1 = 4;
+	$b2 = 3;
+	$altura = 7;
+	
+	echo "El área del trapecio es ".trapecio($b1, $b2, $altura);
+	
+	
+		
+	
 	
 	include("footer.inc");
 ?>
