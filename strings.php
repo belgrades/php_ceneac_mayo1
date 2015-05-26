bfd8a54452e5af3c49f83791cd633276eeac92eb
Fernando Crema <fcremaldc@gmail.com>
2015-05-26 13:24:27 -0430

    Create strings.php

---
+++ strings.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<title>Usando funciones sobre strings</title>
+</head>
+  <h1> Funciones importantes sobre strings  </h1>
+  <h2> chr </h2>
+  <?php
+  	echo chr(64);
+	for($i = 0;$i<256;$i++){
+		echo $i." ".chr($i)."<br>";	
+	}
+  ?>
+<body>
+</body>
+</html>
