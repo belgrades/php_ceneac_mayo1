14bb5a888891454e554a2f5694da7c1aba17f2db
Fernando Crema <fcremaldc@gmail.com>
2015-05-26 15:51:28 -0430

    Update date.php

--- date.php
+++ date.php
@@ -10,9 +10,39 @@
 	$tiempo = time();
 	echo $tiempo."<br>";
 	
-	$fecha = date("H:i:s/d:m:y",mktime(13, 34, 12, 3, 23, 1990));
+	$fecha = date("h:i:s:a/d:m:Y",mktime(13, 34, 12, 3, 3, 1990));
 	
 	echo $fecha;
+?>
+
+<h2> ejemplo restar horas </h2>
+
+<?php
+	function restarHoras($horaini, $horafin){
+		#para la hora inicio
+		list($horai, $mini, $segi) = preg_split("/[: -]/", $horaini);
+		
+		#para la hora fin
+		list($horaf, $minf, $segf) = preg_split("/[: -]/", $horafin);
+		
+		$ini = (($horai*3600) + ($mini*60) + $segi);
+		$fin = (($horaf*3600) + ($minf*60) + $segf);
+		
+		$dif = $fin - $ini;
+		
+		return date("H:i:s", mktime(0,0,$dif));
+			
+	}
+	
+	$hora_ini = "10:05:20";
+	$hora_fin = "04:05:20";
+	
+	echo "La diferencia es ".restarHoras($hora_ini, $hora_fin);
+	
+	function restarNoMilitar($ini, $fin){
+		
+	
+	}
 ?>
 
 </body>
