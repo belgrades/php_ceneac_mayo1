6650981688cee5cbb0d969047c99823b708f1a59
Fernando Crema <fcremaldc@gmail.com>
2015-05-26 13:34:59 -0430

    Update strings.php
    Añadiendo ejemplos con strcmp

--- strings.php
+++ strings.php
@@ -1,15 +1,50 @@
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1">
 <title>Usando funciones sobre strings</title>
 </head>
   <h1> Funciones importantes sobre strings  </h1>
   <h2> chr </h2>
   <?php
   	echo chr(64);
-	for($i = 0;$i<256;$i++){
+	#Caracteres con ISO-8859-1
+	#http://www.w3schools.com/charsets/ref_html_ansi.asp
+	
+	for($i = 0;$i<20;$i++){
 		echo $i." ".chr($i)."<br>";	
 	}
+  ?>
+  
+  <h2> strcmp </h2>
+  <?php
+  	$cad1 = "Fernando";
+	$cad2 = "fernando";
+	
+	echo $cad1." ".$cad2." ".strcmp($cad1, $cad2)."<br>";
+	
+	$cad1 = "fErnando";
+	$cad2 = "fernando";
+	
+	echo $cad1." ".$cad2." ".strcmp($cad1, $cad2)."<br>";
+	
+	$cad1 = "ferna";
+	$cad2 = "fernao";
+	
+	echo $cad1." ".$cad2." ".strcmp($cad1, $cad2)."<br>";
+	
+	$cad1 = "fer";
+	$cad2 = "FER";
+	
+	echo $cad1." ".$cad2." ".strcmp($cad1, $cad2)."<br>";
+	
+	$cad1 = "fernando";
+	$cad2 = "fernando";
+	
+	echo $cad1." ".$cad2." ".strcmp($cad1, $cad2)."<br>";
+	
+	//ejercicio: Dada tres palabras, imprimir de "mayor" a "menor" usando strcmp 
+	
+	
   ?>
 <body>
 </body>
