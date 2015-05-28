4b104a2d640e4f16057a437b5b357abec22350c3
Fernando Crema <fcremaldc@gmail.com>
2015-05-28 13:42:59 -0430

    Create procesar.php

---
+++ procesar.php
@@ -0,0 +1,13 @@
+<html >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<?php
+		echo "Nombre: ".$_GET['nombre']."<br>";
+		echo "Email: ".$_GET['email']."<br>"; 
+	?>
+</body>
+</html>
